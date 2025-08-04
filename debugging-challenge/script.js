c1be18588554d366d5fd4a5cf9191be0c9805012
Jan <jan.walker@phlu.ch>
2025-08-04 10:29:26 +0200

    Remove solution

--- debugging-challenge/script.js
+++
@@ -1,5 +0,0 @@
-console.log("Script geladen");
-
-document.getElementById("clickButton").addEventListener("click", function () {
-    document.getElementById("ausgabe").innerText = "Hallo, Welt!";
-});
